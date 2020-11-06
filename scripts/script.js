87d89fdd98cdf9de0cf1e9a81b11122d4c1a75dc
Aven Zemo <azemo144@gmail.com>
2020-11-06 03:20:19 +0000

    Maldives

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,9 @@ function displayLondon()
   document.getElementById("articleID").innerHTML = "<h1>London</h1>		<p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>	<p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>"
 }
 			
-function displayParis() 
+function displayMaldives() 
 {
-  document.getElementById("articleID").innerHTML = "<h1>Paris</h1><p>Maldives</p>"
+  document.getElementById("articleID").innerHTML = "<h1>Maldives</h1><p>Maldives</p>"
 }
 	
 function displayIao() 
